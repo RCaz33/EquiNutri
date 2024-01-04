45c25c09e01cd7ab31971d1fd980f03693655846
Remi Cazelles <remi.cazelles.fr@gmail.com>
2024-01-04 22:48:32 +0100

    Mise à jour backbone app
    utiliser un K-means pour proposer des alternatives

--- EquiNutri.py
+++ EquiNutri.py
@@ -1,40 +1,74 @@
 import streamlit as st
 
-# Fonction pour effectuer une opération mathématique (exemple : multiplication)
-def f1(x, y, z, selected_options):
-    result = (x * y) + z
-    if 'Square' in selected_options:
-        result *= result
+
+# fonction pour agréger les valeurs de toutes les courses
+def f0(data, liste_de_courses):
+    to_use = data.loc[data.id.isin(liste_de_courses),['protein_100g', 'glucides_100g', 'lipides_100g','serving_portion']]
+    return to_use.protein * to_use.serving_portion, to_use.glucides * to_use.serving_portion, to_use.lipides * to_use.serving_portion
+
+# Fonction pour effectuer calculer l'energie restante
+def f1(protein, glucides, lipides, typologie):
+    result = protein * 12 + glucides * 29 + lipides * 29 
+    if 'Fat' in typologie:
+        result -= 40
+    elif 'Thin' in typologie:
+        result += 20
     return result
 
-# Fonction pour effectuer une autre opération mathématique (exemple : soustraction)
-def f2(x, y, z, selected_options):
-    result = (x - y) * z
-    if 'Cube' in selected_options:
-        result *= result * result
-    return result
+# Fonction pour choisir les éléments de courses 
+def f2(result, typologie):
+    # calculate the amount of each cat to provide
+    list_of_elements = trained_K_means(result, typologie)
+    return list_of_elements
+
+def f3(IMC):
+    if IMC < 16.5:
+        typologie = "Maigreur extrême – dénutrition"
+    elif IMC < 18.5:
+        typologie = "Maigreur"
+    elif IMC < 25:
+        typologie = "Corpulence normale"
+    elif IMC < 30:
+        typologie = "Surpoids ou pré-obésité"
+    elif IMC < 35:
+        typologie = "Obésité modérée (classe I)"
+    elif IMC < 40:
+        typologie = "Obésité sévère (classe II)"
+    else:
+        typologie = "Obésité morbide (classe III)"
+    return typologie
 
 # Fonction principale pour l'interface utilisateur
 def main():
-    st.title("Calculateur Mathématique")
+    st.title("Je termine mes courses")
 
-    # Demander trois entrées entières
-    x = st.number_input("Entrez le premier nombre entier", value=1, step=1)
-    y = st.number_input("Entrez le deuxième nombre entier", value=1, step=1)
-    z = st.number_input("Entrez le troisième nombre entier", value=1, step=1)
+    # Demander trois entrées entières pour definir typologie
+    x = st.number_input("Entrez votre poids en kg", value=1, step=1)
+    y = st.number_input("Entrez votre taille en metre", value=1, step=1)
+    z = st.number_input("Entrez votre age", value=1, step=1)
+    # Attribuer typologie
+    IMC = y / (X**2)
+    typologie = f3(IMC)
 
-    # Demander une entrée avec choix multiples
-    options = ['Square', 'Cube']
-    selected_options = st.multiselect("Sélectionnez les opérations", options)
 
-    # Appeler les fonctions f1 et f2
-    result_f1 = f1(x, y, z, selected_options)
-    result_f2 = f2(x, y, z, selected_options)
+    # Proposer les élements de courses (propre à chaque magasin)
+    options = ['id1', 'id2']
+    selected_options = st.multiselect("Sélectionnez les articles de courses", options)
+    # calculer apport NRJ
+    protein, glucides, lipides = f0(data=,liste_de_courses=selected_options)
+
+
+    # calculer l'energie restante
+    result = f1(protein, glucides, lipides, typologie)
+
+    # proposer suggestion de courses
+    suggestions = f2(result,typologie)
 
     # Afficher les résultats
-    st.header("Résultats:")
-    st.write("Résultat de f1:", result_f1)
-    st.write("Résultat de f2:", result_f2)
+    st.header("Votre liste de courses:")
+    st.write("suivant vos conditions", typologie)
+    st.write("et le contenue de votre liste de course", protein.sum, glucides.sum, lipides.sum)
+    st.write("Nous vous suggérons d'ajouter", suggestions)
 
 # Appeler la fonction principale
 if __name__ == "__main__":
